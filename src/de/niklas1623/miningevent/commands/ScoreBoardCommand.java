364286d9bfa66f5d86306073c34d5ba8d2d3f48c
niklas1623 <71941503+niklas1623@users.noreply.github.com>
2021-01-10 04:56:01 +0100

    Bug fix

--- src/de/niklas1623/miningevent/commands/ScoreBoardCommand.java
+++ src/de/niklas1623/miningevent/commands/ScoreBoardCommand.java
@@ -59,7 +59,7 @@ public class ScoreBoardCommand implements CommandExecutor {
 		Bukkit.getScheduler().runTaskAsynchronously(Main.getInstace(), new Runnable() {
 			public void run() {
 				int place = 0;
-				for (Pair<String, Integer> top : list) {
+				for (Pair<String, Integer> top : list) { 
 					place = place + 1; 
 					Score score = obj.getScore("§7" + place + ". §r" + top.getFirst());
 					score.setScore(top.getSecond());
